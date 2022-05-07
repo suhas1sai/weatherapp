3c3addb705c99a7e68792c9f6e03ba39fe88a5e9
suhas1sai <103729166+suhas1sai@users.noreply.github.com>
2022-05-07 20:45:42 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,54 @@
-const apikey = "3265874a2c77ae4a04bb96236a642d2f";
+let weather = {
+    apiKey: "307d1a74536daa9ce469bde50f0e4d17",
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" +
+                city +
+                "&units=metric&appid=" +
+                this.apiKey
+        )
+            .then((response) => {
+                if (!response.ok) {
+                    alert("No weather found.");
+                    throw new Error("No weather found...");
+                }
+                return response.json();
+            })
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, feels_like, humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector(".city").innerText = name;
+        document.querySelector(".icon").src =
+            "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = Math.round(temp) + "°C";
+        document.querySelector(".feelslike").innerText =
+            "Feels like " + Math.round(feels_like) + "°C";
+        document.querySelector(".humidity").innerText =
+            "Humidity: " + Math.round(humidity) + "%";
+        document.querySelector(".wind").innerText =
+            "Wind speed: " + Math.round(speed) + " km/h";
+        document.querySelector(".weatherLoading").classList.remove("loading");
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".searchBar").value);
+    },
+};
 
-const main = document.getElementById("main");
-const form = document.getElementById("form");
-const search = document.getElementById("search");
-
-const url = (city) =>
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
-
-async function getWeatherByLocation(city) {
-    const resp = await fetch(url(city), { origin: "cors" });
-    const respData = await resp.json();
-
-    console.log(respData);
-
-    addWeatherToPage(respData);
-}
-
-function addWeatherToPage(data) {
-    const temp = KtoC(data.main.temp);
-
-    const weather = document.createElement("div");
-    weather.classList.add("weather");
-
-    weather.innerHTML = `
-        <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /> ${temp}°C <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h2>
-        <small>${data.weather[0].main}</small>
-    `;
-
-    // cleanup
-    main.innerHTML = "";
-
-    main.appendChild(weather);
-}
-
-function KtoC(K) {
-    return Math.floor(K - 273.15);
-}
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    const city = search.value;
-
-    if (city) {
-        getWeatherByLocation(city);
-    }
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
 });
+
+document
+    .querySelector(".searchBar")
+    .addEventListener("keyup", function (event) {
+        if (event.key == "Enter") {
+            weather.search();
+        }
+    });
+
+weather.fetchWeather(" ");
